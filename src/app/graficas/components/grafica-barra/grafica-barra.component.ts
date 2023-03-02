31494da412eaaa31054534d50b6df7335672018e
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-02 00:09:59 -0600

    agregar componente en barras dobles

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,7 +1,45 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
+import { ChartConfiguration, ChartData, ChartType } from "chart.js";
+import { BaseChartDirective } from "ng2-charts";
+import DataLabelsPlugin from 'chartjs-plugin-datalabels';
 
 @Component({
     selector: 'app-grafica-barra',
     templateUrl: 'grafica-barra.component.html'
 })
-export class GraficaBarraComponent { }
+export class GraficaBarraComponent { 
+    @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+    
+    public barChartOptions: ChartConfiguration['options'] = {
+        responsive: true,
+        // We use these empty structures as placeholders for dynamic theming.
+        scales: {
+          x: {},
+          y: {
+            min: 10
+          }
+        },
+        plugins: {
+          legend: {
+            display: true,
+          },
+          datalabels: {
+            anchor: 'end',
+            align: 'end'
+          }
+        }
+      };
+      public barChartType: ChartType = 'bar';
+      public barChartPlugins = [
+        DataLabelsPlugin
+      ];
+    
+      public barChartData: ChartData<'bar'> = {
+        labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
+        datasets: [
+          { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' },
+          { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' },
+          { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series C', backgroundColor:['darkblue'], hoverBackgroundColor: 'red'},
+        ]
+      };
+}
