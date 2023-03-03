ef895035b0fd4ecd492ea94593bde5337dd5f616
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-02 19:33:19 -0600

    añadir flexibilidad al componente de graficas de barras

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,45 +1,55 @@
-import { Component, ViewChild } from "@angular/core";
-import { ChartConfiguration, ChartData, ChartType } from "chart.js";
+import { Component, Input, OnInit, ViewChild } from "@angular/core";
+import { ChartConfiguration, ChartData, ChartDataset, ChartType } from "chart.js";
 import { BaseChartDirective } from "ng2-charts";
 import DataLabelsPlugin from 'chartjs-plugin-datalabels';
 
 @Component({
-    selector: 'app-grafica-barra',
-    templateUrl: 'grafica-barra.component.html'
+  selector: 'app-grafica-barra',
+  templateUrl: 'grafica-barra.component.html'
 })
-export class GraficaBarraComponent { 
-    @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
-    
-    public barChartOptions: ChartConfiguration['options'] = {
-        responsive: true,
-        // We use these empty structures as placeholders for dynamic theming.
-        scales: {
-          x: {},
-          y: {
-            min: 10
-          }
-        },
-        plugins: {
-          legend: {
-            display: true,
-          },
-          datalabels: {
-            anchor: 'end',
-            align: 'end'
-          }
-        }
-      };
-      public barChartType: ChartType = 'bar';
-      public barChartPlugins = [
-        DataLabelsPlugin
-      ];
-    
-      public barChartData: ChartData<'bar'> = {
-        labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
-        datasets: [
-          { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' },
-          { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' },
-          { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series C', backgroundColor:['darkblue'], hoverBackgroundColor: 'red'},
-        ]
-      };
+export class GraficaBarraComponent implements OnInit{
+
+  @Input() horizontal: boolean = false; 
+  @Input() barChartData!: ChartData<'bar'>; 
+  @Input() labels: string[] = [];
+
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+
+  constructor (){
+
+  }
+
+  ngOnInit(): void {
+    if(this.horizontal){
+      this.barChartOptions!.indexAxis = 'y';
+      this.barChartOptions!.scales!["y"]!.min = 0;
+    }   
+    this.barChartData.labels = this.labels;
+  }
+
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: {
+      x: {},
+      y: {
+        min: 10
+      }
+    },
+    plugins: {
+      legend: {
+        display: true,
+      },
+      datalabels: {
+        anchor: 'end',
+        align: 'end'
+      }
+    }
+  };
+  public barChartType: ChartType = 'bar';
+  public barChartPlugins = [
+    DataLabelsPlugin
+  ];
+
+
 }
--- src/app/graficas/pages/barras-doble/barras-doble.component.ts
+++ src/app/graficas/pages/barras-doble/barras-doble.component.ts
@@ -1,6 +1,23 @@
 import { Component } from '@angular/core';
+import { ChartData } from "chart.js";;
 @Component({
     selector: 'app-barras-doble',
     templateUrl: 'barras-doble.component.html'
 })
-export class BarrasDobleComponent { }
+export class BarrasDobleComponent {
+
+    proveedoresLabels: string[] = ['2021', '2022', '2023', '2024', '2025'];
+
+    proveedoresData: ChartData<'bar'> = {
+        datasets: [
+            { data: [100, 200, 300, 400, 500], label: 'Vendedor A' },
+            { data: [50, 250, 30, 450, 200], label: 'Vendedor B' },
+        ]
+    };
+
+    productoData: ChartData<'bar'> = {
+        datasets: [
+            { data: [200, 300, 400, 300, 100], label: 'Carros', backgroundColor: 'blue' },
+        ]
+    };
+}
