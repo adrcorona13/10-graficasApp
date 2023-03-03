cb51571777433320e84ce6e75aa4afdcbd1acf80
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-02 19:49:06 -0600

    crear dona component

--- src/app/graficas/pages/dona/dona.component.ts
+++ src/app/graficas/pages/dona/dona.component.ts
@@ -1,7 +1,33 @@
 import { Component } from "@angular/core";
+import { ChartData, ChartEvent, ChartType } from "chart.js";
 
 @Component({
     selector: 'app-dona',
     templateUrl: 'dona.component.html'
 })
-export class DonaComponent { }
+export class DonaComponent {
+    // Doughnut
+    public doughnutChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+    public doughnutChartData: ChartData<'doughnut'> = {
+        labels: this.doughnutChartLabels,
+        datasets: [
+            { 
+                data: [350, 450, 100],
+                backgroundColor: ['#6405F0','#0724E3', '#05A0F0']
+            },
+            // { data: [50, 150, 120] },
+            // { data: [250, 130, 70] }
+            
+        ],
+    };
+    public doughnutChartType: ChartType = 'doughnut';
+
+    // events
+    public chartClicked({ event, active }: { event: ChartEvent, active: {}[] }): void {
+        console.log(event, active);
+    }
+
+    public chartHovered({ event, active }: { event: ChartEvent, active: {}[] }): void {
+        console.log(event, active);
+    }
+}
